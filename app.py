406fb790eafdc1834c2fa81945ecb0f17205c8f4
Okailey <daniellequaye18@gmail.com>
2024-05-16 17:41:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 from flask import Flask
 from views import views
 
-# #def createMy_app():
-# app  = Flask(__name__)
-# app.register_blueprint(views, url_prefix = "/")
-
-
-# if __name__ == "__main__":
-#     app.run(debug= True, port= 8000)
     
 def create_app():
     app = Flask(__name__)
